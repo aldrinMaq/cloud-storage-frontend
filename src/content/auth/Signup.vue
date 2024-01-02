e0be9deb259938ffe4b35ed2ae6be9fcd94bc3fb
Aldrin <aamaquiling@nemsu.edu.ph>
2024-01-02 08:45:35 +0800

    Revert "fix build errors"
    This reverts commit c047746a6bc84d2e2aee95307719c74ae28a38be.

--- src/content/auth/Signup.vue
+++ src/content/auth/Signup.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-import router from "../../router";
+import router, {baseUrl} from "../../router";
 import {computed, onMounted, ref} from "vue";
 import {defaultUser, User} from "./User.ts";
 import axios, {AxiosError} from "axios";
