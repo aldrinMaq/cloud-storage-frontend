727ce44bb6b643c9040c8c1cfab4c6669ddde895
Aldrin <aamaquiling@nemsu.edu.ph>
2024-05-20 05:58:27 +0800

    update frontend router backend link

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 // export const baseUrl = 'https://cloud-storage-project.onrender.com';
-
-export const baseUrl = 'https://cloud-storage-project.onrender.com';
+// https://cloud-storage-g8vy.onrender.com
+export const baseUrl = 'https://cloud-storage-g8vy.onrender.com';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
